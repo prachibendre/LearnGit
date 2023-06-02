07ca6200de9ab76c429c8962807ad64746f56391
prachibendre <47449429+prachibendre@users.noreply.github.com>
2023-06-02 12:37:27 +0530

    Add files via upload

---
+++ Problem_with_Mutliple_Inheritance.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+class storble
+{
+
+	public:
+
+		storble()
+		{
+			cout<<"In storble Constructor"<<endl;
+		}
+
+		void read()
+		{
+			cout<<"In storble read"<<endl;
+		}
+};
+
+class transmitter : public virtual storble
+{
+
+	public:
+		transmitter()
+		{
+			cout<<"In transmitter Constructor"<<endl;
+		}
+
+};
+
+class receiver : public virtual storble
+{
+
+	public:
+		receiver()
+		{
+			cout<<"In receiver Constructor"<<endl;
+		}
+};
+
+class radio : public transmitter , public receiver
+{
+	public:
+	radio()
+	{
+		cout<<"In radio Constructor"<<endl;
+	
+	}
+};
+
+int main()
+{
+
+	radio rad;
+	rad.read();
+	return 0;
+}
